d94e06414cd7ce250823a371a16792b01bddfc54
Stephen Perez <perezstephenmathew360@gmail.com>
2024-02-03 11:24:56 +0800

    finalize draft part 1
    - landing page
    - log in
    - sign in

---
+++ template/models.php
@@ -0,0 +1,27 @@
+<?php
+
+class Login
+{
+    public $email;
+    public $password;
+
+    public function __construct($email, $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+    }
+}
+
+class SignIn
+{
+    public $name;
+    public $email;
+    public $password;
+
+    public function __construct($name, $email, $password)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+}
---
+++ template/rules.php
@@ -0,0 +1,36 @@
+<?php
+include('models.php');
+
+// function for inserting new data for user creation
+function insertData($name, $email, $password)
+{
+    global $conn;
+    $insertSql = $conn->prepare("INSERT INTO tblUser (name, email, password) VALUES (?, ?, ?)");
+    $insertSql->bind_param("sss", $name, $email, $password);
+
+    if ($insertSql->execute()) {
+        echo "already registed";
+        header('location: login.php');
+        exit();
+    } else {
+        echo $conn->error;
+    }
+}
+
+// function for inserting the users credentials
+function userLogin($email, $password)
+{
+    global $conn;
+    $selectSql = "SELECT email, password FROM tblUser WHERE email = '$email' AND password = '$password'";
+
+    $result = $conn->query($selectSql);
+    if (!empty($email) && !empty($password)) {
+        if ($result->num_rows == 1) {
+
+            header('Location: Home.html');
+            exit();
+        } else {
+            return 'incorrect email or password';
+        }
+    }
+}
